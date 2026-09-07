c38da63dfab775e625a87a4750e93b8a6a62fec6
Shreya <surakshamandyal@gmail.com>
2026-09-07 13:49:27 +0530

    Initial React setup for FinScope

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* We will add Layout and Pages here soon! */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
